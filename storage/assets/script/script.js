313fc78d456264b62ae9f3e0b1c71040d2685495
Cavavalcante-Souza <lucas_cs_sp@hotmail.com>
2018-12-14 00:19:19 -0200

    storage past and JS of home changed

--- storage/assets/script/script.js
+++ storage/assets/script/script.js
@@ -3,10 +3,12 @@ $(document).ready(()=>{
 	$('#menubutton').click(()=>{
 		if(clickbtn){
 	$('#bee').hide();
+	$('#homemain div:first-child').hide();
 	$('#onlybee').hide();
 	clickbtn=false}
 		else{
 			setTimeout(()=>{
+				$('#homemain div:first-child').show();
 				$('#bee').show();
 				$('#onlybee').show()},0300);
 			clickbtn=true;}
